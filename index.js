46fdbb7d93ac422fddf380595f00a6e264bd22f8
Renning Bruns <chopflop@gmail.com>
2018-03-23 07:30:54 -0700

    fixed jsdoc

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const grammar = nearley.Grammar.fromCompiled(myGrammar)
 
 module.exports = {
   /**
-   * Returns a list of possible parsings. The order is not indicative of
-   * confidence or anything like that, instead it's only the order the rules
-   * are written which is usually greedy and thus incomplete. It's my
-   * recommendation that the last result is usually the correct one.
+   * Returns a list of possible parsings as objects with the following
+   * fields:
+   *  - target: who to send the reminder
+   *  - task: what is the reminder
+   *  - moment: momentjs time of when the reminder is due
+   *
+   * The order is not indicative of confidence or anything like that, instead
+   * it's only the order the rules are written which is usually greedy and thus
+   * incomplete. It's my recommendation that the last result is usually the
+   * correct one.
    *
    * The parser also can throw errors when parsings fail. It is important to
    * handle those or else in the case of an IRC bot, things can get very noisy.
    */
+
   parse: input => {
     // parsers are not really reusable (yet)
     const parser = new nearley.Parser(grammar)
-    parser.feed(input);
+    parser.feed(input)
     return parser.results
   }
 }
